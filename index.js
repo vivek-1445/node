0b2013a54366708aef3e1f1304eeb6c1002e95b4
stackzoid <you@example.com>
2021-08-11 13:51:57 +0530

    install ejs and view data on file

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const path = require('path');
 const port = 3000;
 const app = express();
 
 
-app.all('/',function(req,res){
-    //console.log(req.url);
-    
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+app.get('/',function(req,res){
+     res.render('index',{
+         content : "this is index page content"
+     });
+});
+
+app.get('/profile',function(req,res){
+    res.render('profile',{
+        content : "this is profile page content"
+    });
 });
 
 
