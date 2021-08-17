8c18c9516dc1cc5c31cab054b13720b7e559a0af
stackzoid <you@example.com>
2021-08-17 20:03:04 +0530

    insert data using push method

--- index.js
+++ index.js
@@ -1,21 +1,59 @@
 const express = require('express');
 const path = require('path');
+const { getMaxListeners } = require('process');
 const port = 3000;
 const app = express();
+app.use(express.urlencoded());
+app.use(express.static('assets'));
 
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 app.get('/',function(req,res){
      res.render('index',{
-         content : "this is index page content"
+        title : "index",
      });
 });
 
 app.get('/profile',function(req,res){
     res.render('profile',{
-        content : "this is profile page content"
+        title : "profile",
     });
+});
+
+var contactList = [
+    {
+        name : "harsh",
+        mail : "harsh@gmail.com",
+        password : 9393939393
+    },
+    {
+        name : "qwe",
+        mail : "qwe@gmail.com",
+        password : 9393939393
+    },
+    {
+        name : "Vijay",
+        mail : "Vijay@gmail.com",
+        password : 9393939393
+    }
+]
+
+app.get('/view-data',function(req,res){
+    res.render('profile',{
+        title : "profile",
+        Data : contactList
+    });
+});
+
+app.post('/create-contact',function(req,res){
+       
+      contactList.push({
+          name : req.body.name,
+          mail : req.body.mail,
+          password : req.body.psw
+      })
+      return res.redirect('/view-data');
 });
 
 
